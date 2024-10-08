91b786eede6561bbe780f5bc895a82c81ecb1716
Mateus Sanchez <mateo705@hotmail.com>
2024-10-07 19:38:04 -0500

    add: modelo para login.

---
+++ src/main/java/com/produ/model/User.java
@@ -0,0 +1,61 @@
+package com.produ.model;
+
+public class User {
+
+
+    // MODELO PARA LOGIN.
+    private String username;
+    private String password;
+    private String nombre;
+    private String apellido;
+    private String email;
+    private int telefono;
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
--- src/main/java/com/produ/model/model.java
+++
@@ -1,4 +0,0 @@
-package com.produ.model;
-
-public class model {
-}
